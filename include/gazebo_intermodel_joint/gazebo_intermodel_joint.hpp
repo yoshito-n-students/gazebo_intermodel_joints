891643ee88d53de8367a2e647b1d6902b85bd331
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-04-16 17:29:23 +0900

    initial commit of code (roughly tested)

--- include/gazebo_intermodel_joint/gazebo_intermodel_joint.hpp
+++ include/gazebo_intermodel_joint/gazebo_intermodel_joint.hpp
@@ -1,5 +1,9 @@
-#ifndef GAZEBO_GAZEBO_INTERMODEL_JOINT
-#define GAZEBO_GAZEBO_INTERMODEL_JOINT
+#ifndef GAZEBO_INTERMODEL_JOINT
+#define GAZEBO_INTERMODEL_JOINT
+
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #include <gazebo/common/common.hh>
 #include <gazebo/physics/physics.hh>
@@ -8,7 +12,96 @@ namespace gazebo {
 
 class IntermodelJoint : public WorldPlugin {
 public:
-  virtual void Load(physics::WorldPtr world, sdf::ElementPtr sdf) {}
+  virtual void Load(physics::WorldPtr world, sdf::ElementPtr sdf) {
+    // get the joint name from an attribute
+    GZ_ASSERT(sdf->HasAttribute("name"), "No [name] attribute");
+    std::string joint_name(sdf->GetAttribute("name")->GetAsString());
+
+    // resolve the root model of the joint
+    const physics::ModelPtr joint_root(ResolveModel(world, joint_name));
+    GZ_ASSERT(joint_root, "[name] attribute contains an unknown model name");
+
+    // get the joint type from an element. "fixed" is the only supported type for now.
+    GZ_ASSERT(sdf->HasElement("type"), "No [type] element");
+    const std::string joint_type(sdf->Get< std::string >("type"));
+    GZ_ASSERT(joint_type == "fixed", "Value of [type] element is not supported");
+
+    // find the parent link of joint
+    GZ_ASSERT(sdf->HasElement("parent"), "No [parent] element");
+    std::string parent_name(sdf->Get< std::string >("parent"));
+    const physics::ModelPtr parent_root(ResolveModel(world, parent_name));
+    GZ_ASSERT(parent_root, "Value of [parent] element contains an unknown model name");
+    const physics::LinkPtr parent(parent_root->GetLink(parent_name));
+    GZ_ASSERT(parent, "Cannot find a link with the value of [parent] element");
+
+    // find the child link of joint
+    GZ_ASSERT(sdf->HasElement("child"), "No [child] element");
+    std::string child_name(sdf->Get< std::string >("child"));
+    const physics::ModelPtr child_root(ResolveModel(world, child_name));
+    GZ_ASSERT(child_root, "Value of [child] element contains an unknown model name");
+    const physics::LinkPtr child(child_root->GetLink(child_name));
+    GZ_ASSERT(child, "Cannot find a link with the value of [child] element");
+
+    // create the joint
+    const physics::JointPtr joint(joint_root->CreateJoint(joint_name, joint_type, parent, child));
+    GZ_ASSERT(joint, "Cannot create a joint with the given type");
+    joint->Init();
+
+    // done!!
+    std::cout << "[intermodel_joint]: A " << joint_type << "-type joint named "
+              << joint->GetScopedName() << " between the parent link " << parent->GetScopedName()
+              << " and the child link " << child->GetScopedName()
+              << " has been successfully created" << std::endl;
+  }
+
+private:
+  // given:
+  //   - world the entity belongs to
+  //   - entity name containing model names
+  // returns:
+  //   - the parent model
+  //   - entity name without any model names
+  static physics::ModelPtr ResolveModel(const physics::WorldPtr &world, std::string &name) {
+    // pick the root model name from the given name
+    const std::size_t end_root_name(name.find("::"));
+    if (end_root_name == std::string::npos) {
+      // no root model name found
+      return physics::ModelPtr();
+    }
+    const std::string root_name(name.substr(/* begin */ 0, /* len */ end_root_name - 0));
+
+    // find the root model
+    const physics::ModelPtr root_model(world->ModelByName(root_name));
+    if (!root_model) {
+      // no root model found
+      return physics::ModelPtr();
+    }
+
+    // remove the root model name from the given name
+    name.erase(/* begin */ 0, /* len */ (end_root_name + 2) - 0);
+    return ResolveModel(root_model, name);
+  }
+
+  static physics::ModelPtr ResolveModel(const physics::ModelPtr &model, std::string &name) {
+    // pick the nested model name from the given name
+    const std::size_t end_nested_name(name.find("::"));
+    if (end_nested_name == std::string::npos) {
+      // no nested model name found, which means this model is the parent
+      return model;
+    }
+    const std::string nested_name(name.substr(/* begin */ 0, /* len */ end_nested_name - 0));
+
+    // find the nested model
+    const physics::ModelPtr nested_model(model->NestedModel(nested_name));
+    if (!nested_model) {
+      // no nested model found
+      return physics::ModelPtr();
+    }
+
+    // remove the nested model name from the given name
+    name.erase(/* begin */ 0, /* len */ (end_nested_name + 2) - 0);
+    return ResolveModel(nested_model, name);
+  }
 };
 
 } // namespace gazebo
